b7d6c452c1c6930a73d074d2bbfd555f7fdcfa1c
Colin Roche <G00349215@gmit.ie>
2019-02-25 11:46:37 +0000

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home-page', loadChildren: './pages/home-page/home-page.module#HomePagePageModule' },
 ];
 
 @NgModule({
