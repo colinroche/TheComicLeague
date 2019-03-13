308df26674f59a573fb2bcbe9ca6b2530f7ff874
Colin Roche <G00349215@gmit.ie>
2019-03-13 12:53:02 +0000

    file arranging

--- comicApp/src/app/app-routing.module.ts
+++ comicApp/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' },
   { path: 'home-page', loadChildren: './pages/home-page/home-page.module#HomePagePageModule' },
 ];
 
