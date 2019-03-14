2130a71ff26901b0a8c03b8690dbe487f964aef4
Colin Roche <G00349215@gmit.ie>
2019-03-14 11:43:41 +0000

    login-signup html

--- comicApp/src/app/app-routing.module.ts
+++ comicApp/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home-page', pathMatch: 'full' },
-  { path: 'home-page', loadChildren: './pages/home-page/home-page.module#HomePagePageModule' },
+  { path: 'home-page', loadChildren: './pages/home-page/home-page.module#HomePagePageModule' },  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+
 ];
 
 @NgModule({
